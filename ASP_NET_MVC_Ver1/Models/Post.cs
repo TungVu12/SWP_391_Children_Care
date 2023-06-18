8c54e018ce759b3d59e09173ce7b0f3bdeb6e405
Duc Anh <anh.luu1@ntq-solution.com.vn>
2023-06-19 00:35:49 +0700

    Uoload code phần Blog

--- ASP_NET_MVC_Ver1/Models/Post.cs
+++ ASP_NET_MVC_Ver1/Models/Post.cs
@@ -5,13 +5,13 @@ namespace ASP_NET_MVC_Ver1.Models
     public class Post
     {
         [Key]
-        public int PostId { get; set; }
+        public int Id { get; set; }
         [Required]
         public string Title { get; set; }
         [Required]
         public string Description { get; set; }
         public int CreatorId { get; set; }
-        [MaxLength(500)]
-        public string Image { get; set; }
+        public byte[]? Image { get; set; }
+        public DateTime CreateDate { get; set; }
     }
 }
